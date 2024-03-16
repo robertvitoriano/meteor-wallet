7ccf0842766532cdccbe241d254374996df4739f
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-03-17 01:18:51 +0900

    setup tailwind

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,7 +3,7 @@ import { ContactForm } from "./ContactForm";
 import { ContactList } from "./ContactList";
 export const App = () => (
   <div>
-    <h1>Welcome Deployed Meteor Wallet</h1>
+    <h1 className="text-blue-500">Welcome Deployed Meteor Wallet</h1>
     <ContactForm/>
     <ContactList/>
   </div>
