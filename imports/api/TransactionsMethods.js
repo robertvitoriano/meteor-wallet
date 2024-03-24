ec4934d16bf0e379c100e86d472f87a248221243
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-03-24 17:45:51 +0900

    add insert transaction method verifications

--- imports/api/TransactionsMethods.js
+++ imports/api/TransactionsMethods.js
@@ -1,8 +1,10 @@
 import { Meteor } from "meteor/meteor";
 import { check } from "meteor/check";
 import { TransactionsCollection } from "./TransactionsCollection";
+import { WalletsCollection } from "./WalletsCollection";
+import { ContactsCollection } from "./ContactsCollection";
 Meteor.methods({
-  "transactions.insert"({
+  async "transactions.insert"({
     isTransfering,
     sourceWalletId,
     destinationWalletId,
@@ -15,13 +17,24 @@ Meteor.methods({
 
     if (!sourceWalletId) throw new Meteor.Error("Source Wallet is required");
 
-    if (isTransfering && !destinationWalletId)
-      throw new Meteor.Error("Destination wallet is required");
+    if (isTransfering && !destinationWalletId) throw new Meteor.Error("Destination wallet is required");
 
-    if (!amount || amount <= 0)
-      throw new Meteor.Error("provided amount is invalid");
-
-    return TransactionsCollection.insert({
+    if (!amount || amount <= 0) throw new Meteor.Error("provided amount is invalid");
+    
+    const contact = await ContactsCollection.findOneAsync({walletId:destinationWalletId});
+    
+    if(!contact) throw new Meteor.Error("Contact not valid");
+    
+    const sourceWallet = await WalletsCollection.findOneAsync({_id: sourceWalletId})
+    
+    if(sourceWallet.balance < amount) throw new Meteor.Error("not enough balance for this transaction");
+    
+    await WalletsCollection.updateAsync({_id: sourceWalletId},{
+      $set:{
+        balance: sourceWallet.balance - amount 
+      }
+    })
+    await TransactionsCollection.insertAsync({
       type: isTransfering ? 'TRANSFER' : 'ADD',
       amount,
       sourceWalletId,
--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
 import '../imports/api/ContactsCollection';
 import '../imports/api/TransactionsCollection';
 import '../imports/api/TransactionsMethods';
@@ -8,12 +9,20 @@ import '../imports/api/ContactsPublications'
 import '../imports/api/WalletsPublication';
 import { WalletsCollection } from '../imports/api/WalletsCollection';
 
+const walletSchema = new SimpleSchema({
+  balance: Number,
+  currency: String,
+  createdAt: Date,
+});
+
 Meteor.startup(async () => {
   if(WalletsCollection.find().count() === 0){
-    WalletsCollection.insert({
+    const walletData = {
       balance: 15,
       currency: 'USD',
       createdAt: new Date(),
-    })
+    }
+    walletSchema.validate(walletData);
+    WalletsCollection.insert(walletData)
   }
 });
